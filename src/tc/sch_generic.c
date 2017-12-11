67da73b878d5a90c867f5109e066b36ab4a972b9
Lei Chen <raychen@qiyi.com>
2017-12-11 10:20:05 +0800

    tc: fix refcnt check typo when recycling Qsch.

--- src/tc/sch_generic.c
+++ src/tc/sch_generic.c
@@ -100,7 +100,7 @@ static void sch_recycle(void *arg)
 {
     struct Qsch *sch = arg;
 
-    if (!rte_atomic32_read(&sch->refcnt)) {
+    if (rte_atomic32_read(&sch->refcnt)) {
         dpvs_timer_reset(&sch->rc_timer, true);
         RTE_LOG(WARNING, TC, "%s: sch %u is in use.\n", __func__, sch->handle);
         return;
