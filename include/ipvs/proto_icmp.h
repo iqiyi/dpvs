314c7fb894c7a1aaf27f7a7f1991c93ebb92647f
icymoon <tianyan@kuaishou.com>
2018-01-15 14:39:44 +0800

    Add status of icmp session for ipvsadm -lc optimize the unkown status info of tcp/udp sessions.

---
+++ include/ipvs/proto_icmp.h
@@ -0,0 +1,24 @@
+/*
+ * DPVS is a software load balancer (Virtual Server) based on DPDK.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __DP_VS_PROTO_ICMP_H__
+#define __DP_VS_PROTO_ICMP_H__
+
+enum {
+    DPVS_ICMP_S_NORMAL      = 0,
+    DPVS_ICMP_S_LAST
+};
+
+#endif
