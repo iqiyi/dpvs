b193fbbc21f2d3faa63ba9a00b606d8345a193c2
jerrywang <jerrywang@qiyi.com>
2018-04-28 13:54:32 +0800

    conhash: use mtod_offset and mbuf_may_pull to get quic data;

--- src/ipvs/ip_vs_conhash.c
+++ src/ipvs/ip_vs_conhash.c
@@ -25,59 +25,44 @@
 #define QUIC_PACKET_8BYTE_CONNECTION_ID  (1 << 3)
 
 
-/*session id hash target for quic*/
-static int get_quic_hash_target(struct rte_mbuf *mbuf, uint64_t *quic_cid)
+/* QUIC CID hash target for quic*
+ * QUIC CID(qid) should be configured in UDP service*/
+static int get_quic_hash_target(const struct rte_mbuf *mbuf, uint64_t *quic_cid)
 {
     uint8_t pub_flags;
-    uint16_t data_off;
     char *quic_data;
+    uint32_t quic_len;
 
-    data_off = mbuf->data_off;
+    quic_len = ip4_hdrlen(mbuf) + sizeof(struct udp_hdr) + \
+                  sizeof(pub_flags) + sizeof(*quic_cid);
 
-    if (unlikely(NULL == rte_pktmbuf_adj(mbuf,
-              ip4_hdrlen(mbuf) + sizeof(struct udp_hdr)))) {
-        RTE_LOG(ERR, IPVS, "mbuf len is not enough for udp\n");
-        return EDPVS_DPDKAPIFAIL;
-    }
+    if (mbuf_may_pull((struct rte_mbuf *)mbuf, quic_len) != 0)
+        return EDPVS_NOTEXIST;
 
-    if (unlikely(rte_pktmbuf_data_len(mbuf) <
-                (sizeof(pub_flags) + sizeof(*quic_cid)))) {
-        RTE_LOG(ERR, IPVS, "mbuf len is not enough for QUIC\n");
-        goto no_cid;
-    }
-
-    quic_data = rte_pktmbuf_mtod(mbuf, char *);
+    quic_data = rte_pktmbuf_mtod_offset(mbuf, char *, quic_len);
     pub_flags = *((uint8_t *)quic_data);
 
     if ((pub_flags & QUIC_PACKET_8BYTE_CONNECTION_ID) == 0) {
         RTE_LOG(WARNING, IPVS, "packet without cid, pub_flag:%u\n", pub_flags);
-        goto no_cid;
+        return EDPVS_NOTEXIST;
     }
 
     quic_data += sizeof(pub_flags);
-
     *quic_cid = *((uint64_t*)quic_data);
 
-    rte_pktmbuf_prepend(mbuf, mbuf->data_off - data_off);
-
     return EDPVS_OK;
-
-no_cid:
-    rte_pktmbuf_prepend(mbuf, mbuf->data_off - data_off);
-    return EDPVS_NOTEXIST;
 }
 
 /*source ip hash target*/
-static int get_sip_hash_target(struct rte_mbuf *mbuf, uint32_t *sip)
+static int get_sip_hash_target(const struct rte_mbuf *mbuf, uint32_t *sip)
 {
     *sip = ip4_hdr(mbuf)->src_addr;
     return EDPVS_OK;
 }
 
-
 static inline struct dp_vs_dest *
 dp_vs_conhash_get(struct dp_vs_service *svc, struct conhash_s *conhash,
-                  struct rte_mbuf *mbuf)
+                  const struct rte_mbuf *mbuf)
 {
     char str[40] = {0};
     uint64_t quic_cid;
@@ -85,23 +70,26 @@ dp_vs_conhash_get(struct dp_vs_service *svc, struct conhash_s *conhash,
     const struct node_s *node;
 
     if (svc->flags & DP_VS_SVC_F_QID_HASH) {
-        if (EDPVS_OK != get_quic_hash_target(mbuf, &quic_cid)) {
-                /*may not have cid, hash it by sip*/
-            if (EDPVS_OK != get_sip_hash_target(mbuf, &sip))
-                return NULL;
-            snprintf(str, sizeof(str),"%u", sip);
-        }
-        else
-            snprintf(str, sizeof(str),"%lu", quic_cid);
-    }
-
-    else if (svc->flags & DP_VS_SVC_F_SIP_HASH) {
-        if (EDPVS_OK != get_sip_hash_target(mbuf, &sip))
+        if (svc->proto != IPPROTO_UDP) {
+            RTE_LOG(ERR, IPVS, "QUIC cid hash scheduler should only be set in UDP service.\n");
             return NULL;
-        snprintf(str, sizeof(str),"%u", sip);
-    }
+        }
+        /* try to get CID for hash target first, then source IP. */
+        if (EDPVS_OK == get_quic_hash_target(mbuf, &quic_cid)) {
+            snprintf(str, sizeof(str),"%lu", quic_cid);
+        } else if (EDPVS_OK == get_sip_hash_target(mbuf, &sip)) {
+            snprintf(str, sizeof(str),"%u", sip);
+        } else {
+            return NULL;
+        }
+    } else if (svc->flags & DP_VS_SVC_F_SIP_HASH) {
+        if (EDPVS_OK == get_sip_hash_target(mbuf, &sip)) {
+            snprintf(str, sizeof(str),"%u", sip);
+        } else {
+            return NULL;
+        }
 
-    else {
+    } else {
         RTE_LOG(ERR, IPVS, "%s: invalid hash target.\n", __func__);
         return NULL;
     }
@@ -200,7 +188,7 @@ dp_vs_conhash_schedule(struct dp_vs_service *svc, const struct rte_mbuf *mbuf)
 {
     struct dp_vs_dest *dest;
 
-    dest = dp_vs_conhash_get(svc, (struct conhash_s *)(svc->sched_data), (struct rte_mbuf *)mbuf);
+    dest = dp_vs_conhash_get(svc, (struct conhash_s *)svc->sched_data, mbuf);
 
     if (!dest
         || !(dest->flags & DPVS_DEST_F_AVAILABLE)
