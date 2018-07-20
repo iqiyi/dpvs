92974f9b69e6c137fd2fa9f6f05f5bb892733c88
yuwenchao <yuwenchao@qiyi.com>
2018-07-20 21:04:56 +0800

    ipv6/route: lpm6 route table

--- include/route6.h
+++ include/route6.h
@@ -34,16 +34,32 @@ struct route6 {
     struct in6_addr     rt6_gateway;
     struct netif_port   *rt6_dev;
     uint32_t            rt6_mtu;
-    uint32_t            rt6_flags; /* RTF_XXX */
+    uint32_t            rt6_flags;  /* RTF_XXX */
+
+    /* private members */
+    uint32_t            arr_idx;    /* lpm6 array index */
+    struct list_head    hnode;      /* hash list node */
+    rte_atomic32_t      refcnt;
 };
 
 struct route6 *route6_input(struct rte_mbuf *mbuf, struct flow6 *fl6);
 struct route6 *route6_output(struct rte_mbuf *mbuf, struct flow6 *fl6);
 int route6_put(struct route6 *rt);
 
+int route6_add(const struct route6 *rt6);
+int route6_del(const struct route6 *rt6);
+
 int route6_init(void);
 int route6_term(void);
 
-/*test, delete me!*/
-int ipv6_addr_init(void);
+static inline int dump_rt6_prefix(const struct rt6_prefix *rt6_p, char *buf, int len)
+{
+    return snprintf(buf, len, "%X:%X:%X:%X:%X:%X:%X:%X/%d",
+            rt6_p->addr.s6_addr16[0], rt6_p->addr.s6_addr16[1],
+            rt6_p->addr.s6_addr16[2], rt6_p->addr.s6_addr16[3],
+            rt6_p->addr.s6_addr16[4], rt6_p->addr.s6_addr16[5],
+            rt6_p->addr.s6_addr16[6], rt6_p->addr.s6_addr16[7],
+            rt6_p->plen);
+}
+
 #endif /* __DPVS_ROUTE6_H__ */
