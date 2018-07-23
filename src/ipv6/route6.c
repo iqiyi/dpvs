1961437bd697ca20f346c96fd716f48f73bf5f57
Lei Chen <raychen@qiyi.com>
2018-07-23 15:08:47 +0800

    ipv6: stub codes to test IPv6 forwarding

--- src/ipv6/route6.c
+++ src/ipv6/route6.c
@@ -13,6 +13,33 @@
 
 static struct route6 routes[4] = {};
 
+struct neigh {
+    int                 af;
+    union inet_addr     ia;
+    struct ether_addr   ea;
+};
+
+struct neigh neigh_tab[4] = {};
+
+static int ether_addr_pton(const char *ea, struct ether_addr *buf)
+{
+    unsigned int ea_buf[6];
+
+    if (sscanf(ea, "%02x:%02x:%02x:%02x:%02x:%02x",
+           &ea_buf[0], &ea_buf[1], &ea_buf[2],
+           &ea_buf[3], &ea_buf[4], &ea_buf[5]) != 6)
+        return -1;
+
+    buf->addr_bytes[0] = ea_buf[0];
+    buf->addr_bytes[1] = ea_buf[1];
+    buf->addr_bytes[2] = ea_buf[2];
+    buf->addr_bytes[3] = ea_buf[3];
+    buf->addr_bytes[4] = ea_buf[4];
+    buf->addr_bytes[5] = ea_buf[5];
+
+    return 0;
+}
+
 static struct route6 *rt6_lookup(struct rte_mbuf *mbuf, struct flow6 *fl6)
 {
     int i;
@@ -51,6 +78,7 @@ int route6_put(struct route6 *rt)
 int route6_init(void)
 {
     struct route6 *rt;
+    struct neigh *neigh;
 
     rt = &routes[0];
     inet_pton(AF_INET6, "2001:db8::1", &rt->rt6_dst.addr);
@@ -67,6 +95,13 @@ int route6_init(void)
     rt->rt6_flags = RTF_FORWARD;
 
     rt = &routes[2];
+    inet_pton(AF_INET6, "2001:db8:1::1", &rt->rt6_dst.addr);
+    rt->rt6_dst.plen = 128;
+    rt->rt6_dev = netif_port_get_by_name("dpdk0");
+    rt->rt6_mtu = 1500;
+    rt->rt6_flags = RTF_LOCALIN | RTF_HOST;
+
+    rt = &routes[3];
     inet_pton(AF_INET6, "2001:db8:1::", &rt->rt6_dst.addr);
     rt->rt6_dst.plen = 64;
     rt->rt6_dev = netif_port_get_by_name("dpdk0");
@@ -74,13 +109,25 @@ int route6_init(void)
     rt->rt6_mtu = 1500;
     rt->rt6_flags = RTF_FORWARD;
 
-    rt = &routes[3];
-    inet_pton(AF_INET6, "2001:db8:2::", &rt->rt6_dst.addr);
-    rt->rt6_dst.plen = 64;
-    rt->rt6_dev = netif_port_get_by_name("dpdk0");
-    inet_pton(AF_INET6, "2001:db8:2::1", &rt->rt6_gateway);
-    rt->rt6_mtu = 1500;
-    rt->rt6_flags = RTF_FORWARD | RTF_DEFAULT;
+    neigh = &neigh_tab[0];
+    neigh->af = AF_INET6;
+    inet_pton(AF_INET6, "2001:db8::1", &neigh->ia);
+    ether_addr_pton("00:00:00:00:00:00", &neigh->ea);
+
+    neigh = &neigh_tab[1];
+    neigh->af = AF_INET6;
+    inet_pton(AF_INET6, "2001:db8:1::1", &neigh->ia);
+    ether_addr_pton("00:00:00:00:00:00", &neigh->ea);
+
+    neigh = &neigh_tab[2];
+    neigh->af = AF_INET6;
+    inet_pton(AF_INET6, "2001:db8::2", &neigh->ia);
+    ether_addr_pton("00:00:00:00:00:00", &neigh->ea);
+
+    neigh = &neigh_tab[3];
+    neigh->af = AF_INET6;
+    inet_pton(AF_INET6, "2001:db8:1::2", &neigh->ia);
+    ether_addr_pton("00:00:00:00:00:00", &neigh->ea);
 
     return EDPVS_OK;
 }
@@ -94,15 +141,31 @@ int route6_term(void)
 int neigh_output(int af, union inet_addr *nexthop, struct rte_mbuf *mbuf,
                  struct netif_port *dev)
 {
-    struct ether_addr temp, *ea1, *ea2;
+    struct ether_hdr *eh;
+    struct neigh *neigh = NULL;
+    int i;
 
-    /* assume eth header is exist and just need swap src/dst */
-    ea1 = (void *)rte_pktmbuf_prepend(mbuf, sizeof(struct ether_hdr));
-    ea2 = ea1 + 1;
+    eh = (void *)rte_pktmbuf_prepend(mbuf, sizeof(struct ether_hdr));
+    if (!eh) {
+        rte_pktmbuf_free(mbuf);
+        return EDPVS_NOROOM;
+    }
 
-    ether_addr_copy(ea1, &temp);
-    ether_addr_copy(ea2, ea1);
-    ether_addr_copy(&temp, ea2);
+    for (i = 0; i < NELEMS(neigh_tab); i++) {
+        if (inet_addr_equal(AF_INET6, nexthop, &neigh_tab[i].ia)) {
+            neigh = &neigh_tab[i];
+            break;
+        }
+    }
+    if (!neigh) {
+        fprintf(stderr, "%s: no neigh info\n", __func__);
+        rte_pktmbuf_free(mbuf);
+        return EDPVS_INVAL;
+    }
+
+    eh->d_addr = neigh->ea;
+    eh->s_addr = dev->addr;
+    eh->ether_type = htons(ETH_P_IPV6);
 
     return netif_xmit(mbuf, dev);
 }
