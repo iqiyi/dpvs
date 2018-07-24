76e93e86dab436c25e65e7351b09cad580a90231
Lei Chen <raychen@qiyi.com>
2018-07-24 16:58:41 +0800

    ipv6/icmp6: fix icmp6 csum with ext-hdr

--- src/ipv6/icmp6.c
+++ src/ipv6/icmp6.c
@@ -9,14 +9,21 @@
 static inline uint16_t icmp6_csum(struct ip6_hdr *iph, struct icmp6_hdr *ich)
 {
     uint32_t csum, l4_len;
+    struct ip6_hdr hdr;
 
     /* must be linear !! */
     l4_len = ntohs(iph->ip6_plen);
     if ((void *)ich != (void *)(iph + 1))
-        l4_len -= (void *)ich - (void *)iph;
+        l4_len -= (void *)ich - (void *)(iph+1);
+
+    memset(&hdr, 0, sizeof(struct ip6_hdr));
+    hdr.ip6_nxt     = IPPROTO_ICMPV6;
+    hdr.ip6_plen    = htons(l4_len);
+    hdr.ip6_src     = iph->ip6_src;
+    hdr.ip6_dst     = iph->ip6_dst;
 
     csum = rte_raw_cksum(ich, l4_len);
-    csum += rte_ipv6_phdr_cksum((struct ipv6_hdr *)iph, 0);
+    csum += rte_ipv6_phdr_cksum((struct ipv6_hdr *)&hdr, 0);
 
     csum = ((csum & 0xffff0000) >> 16) + (csum & 0xffff);
     csum = (~csum) & 0xffff;
@@ -26,16 +33,16 @@ static inline uint16_t icmp6_csum(struct ip6_hdr *iph, struct icmp6_hdr *ich)
     return csum;
 }
 
-static inline void icmp6_send_csum(struct ip6_hdr *phdr, struct icmp6_hdr *ich)
+static inline void icmp6_send_csum(struct ip6_hdr *shdr, struct icmp6_hdr *ich)
 {
     uint32_t csum, l4_len;
 
     ich->icmp6_cksum = 0;
 
-    l4_len = ntohs(phdr->ip6_plen);
+    l4_len = ntohs(shdr->ip6_plen);
 
     csum = rte_raw_cksum(ich, l4_len);
-    csum += rte_ipv6_phdr_cksum((struct ipv6_hdr *)phdr, 0);
+    csum += rte_ipv6_phdr_cksum((struct ipv6_hdr *)shdr, 0);
 
     csum = ((csum & 0xffff0000) >> 16) + (csum & 0xffff);
     csum = (~csum) & 0xffff;
@@ -50,7 +57,8 @@ static int icmp6_rcv(struct rte_mbuf *mbuf)
     struct ip6_hdr *iph = mbuf->userdata;
     struct icmp6_hdr *ich;
     struct flow6 fl6;
-    struct ip6_hdr phdr;
+    struct ip6_hdr shdr; /* IPv6 header for sending packet */
+    uint32_t icmp_len;
 
     assert(iph);
 
@@ -77,19 +85,23 @@ static int icmp6_rcv(struct rte_mbuf *mbuf)
     if (icmp6_csum(iph, ich) != 0xffff)
         goto drop;
 
+    /* must be linear !! */
+    icmp_len = ntohs(iph->ip6_plen);
+    if ((void *)ich != (void *)(iph + 1))
+        icmp_len -= (void *)ich - (void *)(iph+1);
+
     /* reply */
     ich->icmp6_type = ICMP6_ECHO_REPLY;
 
-    memset(&phdr, 0, sizeof(struct ip6_hdr));
-    phdr.ip6_nxt = IPPROTO_ICMPV6;
-    /* plen is not correct if ext-hdr exist for orig pkt */
-    phdr.ip6_plen = iph->ip6_plen;
+    memset(&shdr, 0, sizeof(struct ip6_hdr));
+    shdr.ip6_nxt = IPPROTO_ICMPV6;
+    shdr.ip6_plen = htons(icmp_len);
     if (!ipv6_addr_is_multicast(&iph->ip6_dst))
-        phdr.ip6_src = iph->ip6_dst; /**/
+        shdr.ip6_src = iph->ip6_dst; /**/
     else
-        inet_pton(AF_INET6, "fe80::1234:56ff:feaa:bbcc", &phdr.ip6_src);
-    phdr.ip6_dst = iph->ip6_src;
-    icmp6_send_csum(&phdr, ich);
+        inet_pton(AF_INET6, "fe80::1234:56ff:feaa:bbcc", &shdr.ip6_src);
+    shdr.ip6_dst = iph->ip6_src;
+    icmp6_send_csum(&shdr, ich);
 
     memset(&fl6, 0, sizeof(struct flow6));
     fl6.fl6_oif = netif_port_get(mbuf->port);
