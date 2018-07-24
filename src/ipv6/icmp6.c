802bf892381e8c96d1ec43bc86cb82c6f2265078
Lei Chen <raychen@qiyi.com>
2018-07-24 13:22:26 +0800

    ipv6: support multicast input.

--- src/ipv6/icmp6.c
+++ src/ipv6/icmp6.c
@@ -25,10 +25,23 @@ static inline uint16_t icmp6_csum(struct ip6_hdr *iph, struct icmp6_hdr *ich)
     return csum;
 }
 
-static inline void icmp6_send_csum(struct ip6_hdr *iph, struct icmp6_hdr *ich)
+static inline void icmp6_send_csum(struct ip6_hdr *phdr, struct icmp6_hdr *ich)
 {
+    uint32_t csum, l4_len;
+
     ich->icmp6_cksum = 0;
-    ich->icmp6_cksum = icmp6_csum(iph, ich);
+
+    l4_len = ntohs(phdr->ip6_plen);
+
+    csum = rte_raw_cksum(ich, l4_len);
+    csum += rte_ipv6_phdr_cksum((struct ipv6_hdr *)phdr, 0);
+
+    csum = ((csum & 0xffff0000) >> 16) + (csum & 0xffff);
+    csum = (~csum) & 0xffff;
+    if (csum == 0)
+        csum = 0xffff;
+
+    ich->icmp6_cksum = csum;
 }
 
 static int icmp6_rcv(struct rte_mbuf *mbuf)
@@ -36,6 +49,7 @@ static int icmp6_rcv(struct rte_mbuf *mbuf)
     struct ip6_hdr *iph = mbuf->userdata;
     struct icmp6_hdr *ich;
     struct flow6 fl6;
+    struct ip6_hdr phdr;
 
     assert(iph);
 
@@ -57,11 +71,25 @@ static int icmp6_rcv(struct rte_mbuf *mbuf)
 
     /* reply */
     ich->icmp6_type = ICMP6_ECHO_REPLY;
-    icmp6_send_csum(iph, ich);
+
+    memset(&phdr, 0, sizeof(struct ip6_hdr));
+    phdr.ip6_nxt = IPPROTO_ICMPV6;
+    /* plen is not correct if ext-hdr exist for orig pkt */
+    phdr.ip6_plen = iph->ip6_plen;
+    if (!ipv6_addr_is_multicast(&iph->ip6_dst))
+        phdr.ip6_src = iph->ip6_dst; /**/
+    else
+        inet_pton(AF_INET6, "fe80::1234:56ff:feaa:bbcc", &phdr.ip6_src);
+    phdr.ip6_dst = iph->ip6_src;
+    icmp6_send_csum(&phdr, ich);
 
     memset(&fl6, 0, sizeof(struct flow6));
     fl6.fl6_oif = netif_port_get(mbuf->port);
-    fl6.fl6_saddr = iph->ip6_dst;
+
+    if (!ipv6_addr_is_multicast(&iph->ip6_dst))
+        fl6.fl6_saddr = iph->ip6_dst;
+    else
+        inet_pton(AF_INET6, "fe80::1234:56ff:feaa:bbcc", &fl6.fl6_saddr);
     fl6.fl6_daddr = iph->ip6_src;
     fl6.fl6_proto = IPPROTO_ICMPV6;
 
