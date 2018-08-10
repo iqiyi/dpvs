feb326082f6e9f6b46c732092c7bdea829e259ba
yuwenchao <yuwenchao@qiyi.com>
2018-08-10 10:51:17 +0800

    ipv6/ipvs: ipvs core support ipv6

--- include/icmp6.h
+++ include/icmp6.h
@@ -1,6 +1,10 @@
 #ifndef __DPVS_ICMPV6_H__
 #define __DPVS_ICMPV6_H__
 
+#include <netinet/icmp6.h>
+#include <netinet/ip6.h>
+
+#define icmp6h_id(icmp6h)        ((icmp6h)->icmp6_dataun.icmp6_un_data16[0])
 void icmp6_send_csum(struct ip6_hdr *iph, struct icmp6_hdr *ich);
 
 int icmpv6_init(void);
