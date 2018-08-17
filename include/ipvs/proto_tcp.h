249f76bff19372bf0dd67fcd3e95ad2c0f317db8
yuwenchao <yuwenchao@qiyi.com>
2018-08-17 18:54:40 +0800

    ipv6/synproxy: ipvs synproxy support ipv6

--- include/ipvs/proto_tcp.h
+++ include/ipvs/proto_tcp.h
@@ -85,6 +85,7 @@ struct tcp_state {
 
 struct tcphdr *tcp_hdr(const struct rte_mbuf *mbuf);
 void tcp4_send_csum(struct ipv4_hdr *iph, struct tcphdr *th);
+void tcp6_send_csum(struct ipv6_hdr *ip6h, struct tcphdr *th);
 struct rte_mempool *get_mbuf_pool(const struct dp_vs_conn *conn, int dir);
 void install_proto_tcp_keywords(void);
 void tcp_keyword_value_init(void);
