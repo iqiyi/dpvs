4da5f3fdd7415868882316b974d4b34ac028db13
ytwang0320 <wyt900320@163.com>
2020-06-15 14:44:43 +0800

    bug_fix: update code

--- tools/keepalived/keepalived/check/check_daemon.c
+++ tools/keepalived/keepalived/check/check_daemon.c
@@ -371,7 +371,7 @@ start_check(list old_checkers_queue, data_t *prev_global_data)
 
 	/* Initialize IPVS topology */
 	if (!init_services())
-		stop_check(KEEPALIVED_EXIT_FATAL);
+	    stop_check(KEEPALIVED_EXIT_FATAL);
 	
 	/* Dump configuration */
 	if (__test_bit(DUMP_CONF_BIT, &debug))
