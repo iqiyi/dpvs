6b30fff5adb0c456cc90bc60ba3d1f790163964e
Aaron LI <aly@aaronly.me>
2020-08-24 02:35:11 -0700

    kmod/toa: Fix compilation on newer systems (e.g., CentOS 7.7+)
    On newer systems (such as CentoOS 7.7+), the <linux/netfilter.h> header
    must be explicitly included to obtain the declaration of
    'struct nf_sockopt_ops'.  Otherwise, the toa modules fail to compile.
    
    This fixes issue #504.

--- kmod/toa/toa.h
+++ kmod/toa/toa.h
@@ -20,6 +20,9 @@
 #include <net/ipv6.h>
 #include <net/transp_v6.h>
 #include <net/sock.h>
+#ifdef TOA_NAT64_ENABLE
+#  include <linux/netfilter.h>  /* for 'struct nf_sockopt_ops' */
+#endif
 
 #define TOA_VERSION "2.0.0.0"
 
