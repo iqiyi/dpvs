iqiyi/dpvs

commit 28d97b73ce8859eca9c97d5524cfa3398c37f68f

Use the same RTE_MACHINE and CPU CFLAGS as DPDK
DPVS was hardcoding RTE_MACHINE=native and CPU CFLAGS in 'src/dpdk.mk'.
However, these can be different from the values chosen by DPDK, which
may be a potential issue.  Especially, the 'RTE_MACHINE=native' CFLAG
is preventing a portable build.  For example, I couldn't build DPVS on
one machine and deploy it on another machine when their CPU generation
is different (DPVS is unable to run due to 'illegal instruction' error).

So, it's better to include '$(RTE_SDK)/mk/rte.vars.mk' and then DPVS
would be built with the same RTE_MACHINE and CPU CFLAGS as DPDK.  This
is also used by DPDK examples.

In addition, use '$(Q)' (provided by 'rte.vars.mk') instead of '@' in
'src/Makefile', which allows us to pass 'V=1' to make to show verbose
build commands, similar to the DPDK build.

src/Makefile
@@ -34,8 +34,8 @@ DATE_STRING := $(shell date +%Y.%m.%d.%H:%M:%S)
 # same path of THIS Makefile
 SRCDIR := $(dir $(realpath $(firstword $(MAKEFILE_LIST))))
 
-include $(SRCDIR)/config.mk
 include $(SRCDIR)/dpdk.mk
+include $(SRCDIR)/config.mk
 
 INCDIRS += -I $(SRCDIR)/../include
 
@@ -67,12 +67,12 @@ OBJS := $(patsubst %.c,%.o,$(OBJS))
 all: $(TARGET)
 
 $(TARGET): $(OBJS)
-	@$(CC) $(CFLAGS) $^ -o $@
 	@echo "  $(notdir $@)"
+	$(Q)$(CC) $(CFLAGS) $^ -o $@
 
 %.o: %.c
-	@$(CC) -c $(CFLAGS) $< -o $@
 	@echo "  $(notdir $@)"
+	$(Q)$(CC) -c $(CFLAGS) $< -o $@
 
 clean:
 	find $(SRCDIR) -name '*.o' | xargs rm -f

src/dpdk.mk
@@ -25,16 +25,9 @@ DPDKDIR := $(RTE_SDK)/$(RTE_TARGET)
 
 INCDIRS += -I $(DPDKDIR)/include
 
-CFLAGS += -include $(DPDKDIR)/include/rte_config.h
+include $(RTE_SDK)/mk/rte.vars.mk
 
-CFLAGS += -march=native \
-		  -DRTE_MACHINE_CPUFLAG_SSE \
-		  -DRTE_MACHINE_CPUFLAG_SSE2 \
-		  -DRTE_MACHINE_CPUFLAG_SSE3 \
-		  -DRTE_MACHINE_CPUFLAG_SSSE3 \
-		  -DRTE_MACHINE_CPUFLAG_SSE4_1 \
-		  -DRTE_MACHINE_CPUFLAG_SSE4_2 \
-		  -DRTE_COMPILE_TIME_CPUFLAGS=RTE_CPUFLAG_SSE,RTE_CPUFLAG_SSE2,RTE_CPUFLAG_SSE3,RTE_CPUFLAG_SSSE3,RTE_CPUFLAG_SSE4_1,RTE_CPUFLAG_SSE4_2
+CFLAGS += -include $(DPDKDIR)/include/rte_config.h
 
 LIBS += -L $(DPDKDIR)/lib
 

src/main.c
@@ -15,7 +15,15 @@
  * GNU General Public License for more details.
  *
  */
+
+/*
+ * '_GNU_SOURCE' has been defined in newer DPDK's makefile
+ * (e.g., 18.11) but not in order DPDK (e.g., 17.11).
+ */
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
+
 #include <pthread.h>
 #include <assert.h>
 #include <getopt.h>

commit 8ed56bbcbac0f9ef2355cfa87e2c2242cdaa18a6

bugfix: fix  not working problem introduced by commit 28d97b7(pr#667)

src/Makefile
@@ -34,8 +34,13 @@ DATE_STRING := $(shell date +%Y.%m.%d.%H:%M:%S)
 # same path of THIS Makefile
 SRCDIR := $(dir $(realpath $(firstword $(MAKEFILE_LIST))))
 
-include $(SRCDIR)/dpdk.mk
+ifeq ($(RTE_SDK),)
+	$(error "The variable RTE_SDK is not defined.")
+endif
+include $(RTE_SDK)/mk/rte.vars.mk
+
 include $(SRCDIR)/config.mk
+include $(SRCDIR)/dpdk.mk
 
 INCDIRS += -I $(SRCDIR)/../include
 

src/dpdk.mk
@@ -25,8 +25,6 @@ DPDKDIR := $(RTE_SDK)/$(RTE_TARGET)
 
 INCDIRS += -I $(DPDKDIR)/include
 
-include $(RTE_SDK)/mk/rte.vars.mk
-
 CFLAGS += -include $(DPDKDIR)/include/rte_config.h
 
 LIBS += -L $(DPDKDIR)/lib