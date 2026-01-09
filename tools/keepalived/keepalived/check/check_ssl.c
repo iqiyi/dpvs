50abf6216887d67e39cc39eea366c02a2a39cd4d
ytwang0320 <wyt900320@163.com>
2026-01-09 16:59:57 +0800

    keepalived: use EVP API instead of deprecated MD5

--- tools/keepalived/keepalived/check/check_ssl.c
+++ tools/keepalived/keepalived/check/check_ssl.c
@@ -27,6 +27,8 @@
 
 #include <fcntl.h>
 #include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/md5.h>
 
 #include "check_ssl.h"
 #include "check_api.h"
@@ -315,8 +317,11 @@ ssl_read_thread(thread_ref_t thread)
 	} else if (req->error) {
 
 		/* All the SSL streal has been parsed */
-		if (url->digest)
-			MD5_Final(digest, &req->context);
+		if (url->digest) {
+			EVP_DigestFinal_ex(req->context, digest, NULL);
+			EVP_MD_CTX_free(req->context);
+			req->context = NULL;
+		}
 		SSL_set_quiet_shutdown(req->ssl, 1);
 
 		r = (req->error == SSL_ERROR_ZERO_RETURN) ? SSL_shutdown(req->ssl) : 0;
